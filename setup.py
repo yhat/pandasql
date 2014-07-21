6346178cc2e90c5272442d5eecac4f62387fa3fd
Greg Lamp <lamp.greg@gmail.com>
2014-07-21 18:18:00 -0400

    bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ required = []
 
 setup(
     name="pandasql",
-    version="0.6.0",
+    version="0.6.1",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/pandasql/",
